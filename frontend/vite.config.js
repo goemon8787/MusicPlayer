66e3f23c6d610e0d3ddb44b1855741a9e09f370b
goemon8787 <niceopchan@gmail.com>
2023-07-23 23:40:44 +0900

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [
     vue({
       template: { transformAssetUrls },
@@ -28,6 +29,7 @@ export default defineConfig({
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
   server: {
-    port: 3000,
+    host: true,
+    port: 8081,
   },
 });
